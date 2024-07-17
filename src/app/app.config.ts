94972aa8472a57f11f607b9deceb4b04d91f6aea
David Maslonka <maslonka.david@gmail.com>
2024-07-17 17:06:43 +0200

    Added translation.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,40 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { HttpClient, provideHttpClient } from '@angular/common/http'
+import {
+  APP_INITIALIZER,
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core'
+import { provideRouter } from '@angular/router'
 
-import { routes } from './app.routes';
+import { firstValueFrom } from 'rxjs'
+
+import { TranslateLoader, TranslateModule, TranslateService } from '@ngx-translate/core'
+import { TranslateHttpLoader } from '@ngx-translate/http-loader'
+
+import { routes } from './app.routes'
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
-};
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        loader: {
+          provide: TranslateLoader,
+          useFactory: (httpClient: HttpClient) =>
+            new TranslateHttpLoader(httpClient, '/public/i18n/', '.json'),
+          deps: [HttpClient],
+        },
+        defaultLanguage: 'sk',
+      }),
+    ),
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (translate: TranslateService) => () => firstValueFrom(translate.use('sk')),
+      deps: [TranslateService],
+      multi: true,
+    },
+  ],
+}
